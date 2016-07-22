e1a29d1af6c3037ff86b512e8f95a4b31aca18dc
David Whale <david@thinkingbinaries.com>
2016-07-22 20:23:48 +0100

    Added placeholder config.py for later global config options

---
+++ config.py
@@ -0,0 +1,20 @@
+# config.py  22/07/2016  D.J.Whale
+#
+# Placeholder for machine global configuration values.
+#
+# May also have code in future to read and parse command line options
+# and an external config file, to override config settings.
+
+# candidates:
+
+# assembler.PREFIX_ADDR
+# io.thebase (DECIMAL, HEXADECIMAL, BINARY)
+# hexadecimal.DEFAULT_WIDTH
+# decimal.DEFAULT_WIDTH
+# binary.DEFAULT_WIDTH
+# interactive (size of simulator memory)
+# simulator.BUS_MAX
+# simulator.PC_MAX
+
+
+# END
