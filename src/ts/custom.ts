19ffb1b3bc262ba25cd61c46f0173e78562d88f5
David Whale <david@thinkingbinaries.com>
2017-04-29 14:22:13 +0100

    function signatures for mlc_decimal

--- src/ts/custom.ts
+++ src/ts/custom.ts
@@ -287,63 +287,67 @@ namespace mlc_io {
 
 namespace mlc_decimal {
 
-// # Read and write decimal 3 digit unsigned numbers
-// # with zero padding
-// 
-// DEFAULT_WIDTH = 3
-// 
-// def read(width=None, file=None):
-//     #print("read")
-//     """return a decimal number in range 000-999"""
-//     # default width is 3 characters, but you can ask for wider
-// 
-//     if width == None:
-//         width = DEFAULT_WIDTH
-// 
-//     if file == None: # stdin, strip blank lines
-//         #print("stdin")
-//         while True:
-//             try:
-//                 try:
-//                     line = raw_input()
-//                 except:
-//                     line = input()
-//             except EOFError:
-//                 #print(" EOF")
-//                 return None # EOF
-// 
-//             line = line.strip() # strip wrapping spaces and newline char
-//             if len(line) != 0:
-//                 instr = int(line)
-//                 #print(" instr:" + str(instr))
-//                 return instr
-// 
-//     else: # from file, strip blank lines
-//         #print("file")
-//         #raise RuntimeError("HERE")
-//         while True:
-//             line = file.readline()
-//             if line == "":
-//                 #print(" EOF")
-//                 return None # EOF
-//             line = line.strip() # strip wrapping spaces and newline char
-//             if len(line) != 0:
-//                 instr = int(line)
-//                 #print(" instr:" + str(instr))
-//                 return instr
-// 
-// 
-// def write(number, width=None, file=None):
-//     #print("write: %s %s %s %s" %( str(number) , str(type(number)), str(width), str(type(width))))
-//     """write a decimal number 000-999 zero padded"""
-// 
-//     if width == None:
-//         width = DEFAULT_WIDTH
-// 
-//     if file == None: # stdout
-//         print(str(number).zfill(width))
-//     else: # to file
-//         file.write(str(number).zfill(width) + "\n")
+    // # Read and write decimal 3 digit unsigned numbers
+    // # with zero padding
+    //
+    // DEFAULT_WIDTH = 3
+
+    function read(width:int=none, file:string=none):void {
+        // def read(width=None, file=None):
+        //     #print("read")
+        //     """return a decimal number in range 000-999"""
+        //     # default width is 3 characters, but you can ask for wider
+        //
+        //     if width == None:
+        //         width = DEFAULT_WIDTH
+        //
+        //     if file == None: # stdin, strip blank lines
+        //         #print("stdin")
+        //         while True:
+        //             try:
+        //                 try:
+        //                     line = raw_input()
+        //                 except:
+        //                     line = input()
+        //             except EOFError:
+        //                 #print(" EOF")
+        //                 return None # EOF
+        //
+        //             line = line.strip() # strip wrapping spaces and newline char
+        //             if len(line) != 0:
+        //                 instr = int(line)
+        //                 #print(" instr:" + str(instr))
+        //                 return instr
+        //
+        //     else: # from file, strip blank lines
+        //         #print("file")
+        //         #raise RuntimeError("HERE")
+        //         while True:
+        //             line = file.readline()
+        //             if line == "":
+        //                 #print(" EOF")
+        //                 return None # EOF
+        //             line = line.strip() # strip wrapping spaces and newline char
+        //             if len(line) != 0:
+        //                 instr = int(line)
+        //                 #print(" instr:" + str(instr))
+        //                 return instr
+        return 0 //TODO
+    }
+
+    function write(number:int, width:int=none, file:string=none):void {
+        // def write(number, width=None, file=None):
+        //     #print("write: %s %s %s %s" %( str(number) , str(type(number)), str(width), str(type(width))))
+        //     """write a decimal number 000-999 zero padded"""
+        //
+        //     if width == None:
+        //         width = DEFAULT_WIDTH
+        //
+        //     if file == None: # stdout
+        //         print(str(number).zfill(width))
+        //     else: # to file
+        //         file.write(str(number).zfill(width) + "\n")
+    }
 }
 
 
