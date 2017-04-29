b285b7421c452e3b86ec8c59145e8baeb385006c
David Whale <david@thinkingbinaries.com>
2017-04-29 14:33:03 +0100

    Function signatures for mlc_ext_instrs

--- src/ts/custom.ts
+++ src/ts/custom.ts
@@ -492,36 +492,38 @@ namespace mlc_io_instrs {
 
 namespace mlc_ext_instrs {
 
-// # Extended instructions that are added to the instuction set simulator
-// 
-// # Runtime registration of new mnemonics in the instruction table
-// # This ensures that the assembler and disassembler can use the instructions
-// # with their proper mnemonic names, but by runtime registering them int
-// # does not pollute instruction.py with extended functionality
-// 
-// instruction.registerMnemonic("EXT", 400, False)
-// instruction.registerMnemonic("USB", 401, False)
-// instruction.registerMnemonic("MUL", 402, False)
-// instruction.registerMnemonic("DIV", 403, False)
-// 
-// 
-// def execExtendedInstr(operand, acc):
-// 	"""Execute any user instructions here (instruction.X_xx)"""
-// 
-// 	if   operand == instruction.getOperand(instruction.USB): # Use Breg in next instruction
-// 		extarch.b_flag = True # next instr will use B instead of A
-// 
-// 	elif operand == instruction.getOperand(instruction.MUL): # multiply
-// 		acc = extarch.b_reg * acc
-// 
-// 	elif operand == instruction.getOperand(instruction.DIV): # divide
-// 		##trace("acc %d breg %d" % (acc, b_reg))
-// 		acc = extarch.b_reg / acc
-// 
-// 	else:
-// 		raise ValueError("Unknown EXT instr:" + str(operand))
-// 
-// 	return acc
+    // # Extended instructions that are added to the instuction set simulator
+    //
+    // # Runtime registration of new mnemonics in the instruction table
+    // # This ensures that the assembler and disassembler can use the instructions
+    // # with their proper mnemonic names, but by runtime registering them int
+    // # does not pollute instruction.py with extended functionality
+    //
+    // instruction.registerMnemonic("EXT", 400, False)
+    // instruction.registerMnemonic("USB", 401, False)
+    // instruction.registerMnemonic("MUL", 402, False)
+    // instruction.registerMnemonic("DIV", 403, False)
+
+    function exec_extended_instr(operand:int, acc:int):int {
+        // def execExtendedInstr(operand, acc):
+        // 	"""Execute any user instructions here (instruction.X_xx)"""
+        //
+        // 	if   operand == instruction.getOperand(instruction.USB): # Use Breg in next instruction
+        // 		extarch.b_flag = True # next instr will use B instead of A
+        //
+        // 	elif operand == instruction.getOperand(instruction.MUL): # multiply
+        // 		acc = extarch.b_reg * acc
+        //
+        // 	elif operand == instruction.getOperand(instruction.DIV): # divide
+        // 		##trace("acc %d breg %d" % (acc, b_reg))
+        // 		acc = extarch.b_reg / acc
+        //
+        // 	else:
+        // 		raise ValueError("Unknown EXT instr:" + str(operand))
+        //
+        // 	return acc
+        return 0 //TODO
+    }
 }
 
 
