447992400326c429345413a9616d96ee7164491d
David Whale <david@thinkingbinaries.com>
2017-04-29 14:30:46 +0100

    Function signatures for mlc_hexadecimal

--- src/ts/custom.ts
+++ src/ts/custom.ts
@@ -388,27 +388,31 @@ namespace mlc_binary {
 
 namespace mlc_hexadecimal {
 
-// # Read and write hexadecimal 8 bit (2 char) or 16 bit (4 char) unsigned numbers
-// # with zero padding
-// 
-// DEFAULT_WIDTH=4
-//  
-// def read(file=None):
-//     if file == None:
-//         v = raw_input()
-//     else:
-//         v = file.readline()
-//     return int(v, 16)
-//  
-// 
-// def write(number, width=DEFAULT_WIDTH, file=None):
-//     format = "%%0%dX" % width
-//     v = format % number
-//     if file == None:
-//         print(v)
-//     else:
-//         file.write(v)
-//         file.write('\n')
+    // # Read and write hexadecimal 8 bit (2 char) or 16 bit (4 char) unsigned numbers
+    // # with zero padding
+    //
+    // DEFAULT_WIDTH=4
+
+    function read(file:string=none):int {
+        // def read(file=None):
+        //     if file == None:
+        //         v = raw_input()
+        //     else:
+        //         v = file.readline()
+        //     return int(v, 16)
+        return 0 //TODO
+    }
+
+    function write(number:int, width:int=DEFAULT_WIDTH, file:string=none):void {
+        // def write(number, width=DEFAULT_WIDTH, file=None):
+        //     format = "%%0%dX" % width
+        //     v = format % number
+        //     if file == None:
+        //         print(v)
+        //     else:
+        //         file.write(v)
+        //         file.write('\n')
+    }
 }
 
 
