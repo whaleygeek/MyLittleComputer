5d7d7c85b3c788651cb73e365ce131e6f6c7d919
David Whale <david@thinkingbinaries.com>
2017-04-29 14:41:52 +0100

    Function signatures for mlc_shell

--- src/ts/custom.ts
+++ src/ts/custom.ts
@@ -897,38 +897,38 @@ namespace mlc_loader {
 }
 
 
-//----- INTERACTIVE -----------------------------------------------------------
+//----- SHELL -----------------------------------------------------------------
 
 namespace mlc_shell {
+    // # interactive shell
 
-// # interactive shell
-//
-// def todec(n):
-// 	return str(n).zfill(3)
-//
-// simulator.memory = [0 for i in range(99)]
-//
-// def main():
-// 	while not simulator.halt_flag:
-// 		line = raw_input("instruction? ")
-//
-// 		label, operator, operand, labelref = parser.parseLine(line)
-// 		instr = instruction.build(operator, operand)
-// 		print("instr:" + instruction.toString(instr))
-//
-// 		simulator.memory[simulator.program_counter] = instr
-// 		simulator.cycle()
-//
-// 		#TODO: Somehow adding b_reg functionality needs to add b display here too?
-// 		print("  pc:"    + todec(simulator.program_counter)
-// 			+ " a:"    + todec(simulator.accumulator)
-// 			+ " z:"    + str(simulator.z_flag)
-// 			+ " p:"    + str(simulator.p_flag)
-// 			+ " halt:" + str(simulator.halt_flag))
-//
-//
-// if __name__ == "__main__":
-// 	main()
+    // simulator.memory = [0 for i in range(99)]
+
+    function to_dec(n:int):string {
+        // def todec(n):
+        // 	return str(n).zfill(3)
+        return "" //TODO
+    }
+
+    function main(void):void {
+        // def main():
+        // 	while not simulator.halt_flag:
+        // 		line = raw_input("instruction? ")
+        //
+        // 		label, operator, operand, labelref = parser.parseLine(line)
+        // 		instr = instruction.build(operator, operand)
+        // 		print("instr:" + instruction.toString(instr))
+        //
+        // 		simulator.memory[simulator.program_counter] = instr
+        // 		simulator.cycle()
+        //
+        // 		#TODO: Somehow adding b_reg functionality needs to add b display here too?
+        // 		print("  pc:"    + todec(simulator.program_counter)
+        // 			+ " a:"    + todec(simulator.accumulator)
+        // 			+ " z:"    + str(simulator.z_flag)
+        // 			+ " p:"    + str(simulator.p_flag)
+        // 			+ " halt:" + str(simulator.halt_flag))
+    }
 }
 
 
