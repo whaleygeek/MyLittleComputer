ffa680b7621eb62b20542e42d140e1f0aad15671
David Whale <david@thinkingbinaries.com>
2017-04-29 14:32:25 +0100

    Function signatures for mlc_io_instrs

--- src/ts/custom.ts
+++ src/ts/custom.ts
@@ -455,33 +455,36 @@ namespace mlc_hlt_instrs {
 
 namespace mlc_io_instrs {
 
-// # Add extra IO instructions to the instruction set simulator
-// // 
-// # Runtime registration of generic form
-// # you can add specific mnemonic names here if you want,
-// # see the example in extinstrs.py to see how to do it in a way that
-// # does not pollute instruction.py with optional features.
-// // 
-// instruction.registerMnemonic("IO", 900, False)
-// // 
-// 
-// # INP and OUT are already handled in simulator.py, and never delegated here.
-// // 
-// # 901=INP and 902=OUT, but 900 and 903..999 are not used.
-// # These undefined instructions are therefore useful to use to define
-// # other types of IO, such as GPO for general purpose output, GPI for
-// # general purpose input, AIN for analog input, and other I/O
-// # related things that would not warrant a HLT for an OS call.
-// # they could be done as User instrutions (U) but it's handy to group
-// # all the I/O together into a set of instructions.
-// // 
-// 
-// def execIOInstr(operand, acc):
-// 	"""Execute any user IO instructions here (instruction.IO_xx)"""
-// // 
-// 	print("exec IO instr %d" % str(operand))
-// 	# DEFINE NEW IO INSTRUCTIONS HERE
-// 	return acc
+    // # Add extra IO instructions to the instruction set simulator
+    // //
+    // # Runtime registration of generic form
+    // # you can add specific mnemonic names here if you want,
+    // # see the example in extinstrs.py to see how to do it in a way that
+    // # does not pollute instruction.py with optional features.
+    // //
+    // instruction.registerMnemonic("IO", 900, False)
+    // //
+    //
+    // # INP and OUT are already handled in simulator.py, and never delegated here.
+    // //
+    // # 901=INP and 902=OUT, but 900 and 903..999 are not used.
+    // # These undefined instructions are therefore useful to use to define
+    // # other types of IO, such as GPO for general purpose output, GPI for
+    // # general purpose input, AIN for analog input, and other I/O
+    // # related things that would not warrant a HLT for an OS call.
+    // # they could be done as User instrutions (U) but it's handy to group
+    // # all the I/O together into a set of instructions.
+
+    function exec_IO_instr(operand:int, acc:int):int {
+        //
+        // def execIOInstr(operand, acc):
+        // 	"""Execute any user IO instructions here (instruction.IO_xx)"""
+        // //
+        // 	print("exec IO instr %d" % str(operand))
+        // 	# DEFINE NEW IO INSTRUCTIONS HERE
+        // 	return acc
+        return 0 //TODO
+    }
 }
 
 
