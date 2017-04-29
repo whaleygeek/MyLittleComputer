038b6615dc5a31344401ef174e5ff0e56e3edce5
David Whale <david@thinkingbinaries.com>
2017-04-29 14:39:19 +0100

    First guess at function signatures for mlc_parser Returning multiple values needs looking at

--- src/ts/custom.ts
+++ src/ts/custom.ts
@@ -783,75 +783,80 @@ namespace mlc_simulator {
 
 namespace mlc_parser {
 
-// # Parse an input program file
-//
-// def labelFromString(s):
-// 	"""Work out if this operand is a label or not"""
-//
-// 	# Is it numeric?
-// 	try:
-// 		operand = int(s)
-// 		return operand, None # just a normal number
-// 	except:
-// 		pass
-//
-// 	# Must be a label
-// 	return None, s # A labelref
-//
-//
-// def parseLine(line):
-// 	"""parse a line into an instruction"""
-//
-// 	# Ignore lines that are comments
-// 	line = line.strip()
-// 	if line.startswith('#'):
-// 		return None, None, None, None # whole-line comment
-//
-// 	# Strip off end of line comment
-// 	try:
-// 		commentpos = line.index('#')
-// 		line = line[:commentpos]
-// 		line = line.strip()
-// 	except:
-// 		pass
-//
-// 	# Ignore lines with no instruction on them
-// 	parts    = line.split(" ")
-// 	if len(parts) == 0: # empty line
-// 		return None
-//
-// 	# Split line into [label] [operator] [operand]
-// 	label    = None
-// 	operator = None
-// 	operand  = None
-// 	labelref = None
-//
-// 	if len(parts) == 1: # (label) or (operator)
-// 		if instruction.isOperator(parts[0]): # (operator)
-// 			operator = instruction.operatorFromString(parts[0])
-// 		else: # (label) (operator)
-// 			label    = parts[0]
-//
-// 	elif len(parts) == 2: # (label operator) or (operator operand)
-// 		if instruction.isOperator(parts[0]): # (operator operand)
-// 			operator          = instruction.operatorFromString(parts[0])
-// 			operand, labelref = labelFromString(parts[1])
-//
-// 		else: # (label operator)
-// 			label    = parts[0]
-// 			operator = instruction.operatorFromString(parts[1])
-//
-// 	elif len(parts) == 3: # (label operator operand)
-// 		label             = parts[0]
-// 		operator          = instruction.operatorFromString(parts[1])
-// 		operand, labelref = labelFromString(parts[2])
-//
-// 	# DAT or instruction?
-// 	if operator == instruction.DAT:
-// 		operator = operand
-// 		operand  = None
-//
-// 	return label, operator, operand, labelref
+    // # Parse an input program file
+
+    function label_from_string(s:string):int {
+        // def labelFromString(s):
+        // 	"""Work out if this operand is a label or not"""
+        //
+        // 	# Is it numeric?
+        // 	try:
+        // 		operand = int(s)
+        // 		return operand, None # just a normal number
+        // 	except:
+        // 		pass
+        //
+        // 	# Must be a label
+        // 	return None, s # A labelref
+        return none //TODO
+    }
+
+    function parse_line(line:string):list_of_stuff   {
+        // def parseLine(line):
+        // 	"""parse a line into an instruction"""
+        //
+        // 	# Ignore lines that are comments
+        // 	line = line.strip()
+        // 	if line.startswith('#'):
+        // 		return None, None, None, None # whole-line comment
+        //
+        // 	# Strip off end of line comment
+        // 	try:
+        // 		commentpos = line.index('#')
+        // 		line = line[:commentpos]
+        // 		line = line.strip()
+        // 	except:
+        // 		pass
+        //
+        // 	# Ignore lines with no instruction on them
+        // 	parts    = line.split(" ")
+        // 	if len(parts) == 0: # empty line
+        // 		return None
+        //
+        // 	# Split line into [label] [operator] [operand]
+        // 	label    = None
+        // 	operator = None
+        // 	operand  = None
+        // 	labelref = None
+        //
+        // 	if len(parts) == 1: # (label) or (operator)
+        // 		if instruction.isOperator(parts[0]): # (operator)
+        // 			operator = instruction.operatorFromString(parts[0])
+        // 		else: # (label) (operator)
+        // 			label    = parts[0]
+        //
+        // 	elif len(parts) == 2: # (label operator) or (operator operand)
+        // 		if instruction.isOperator(parts[0]): # (operator operand)
+        // 			operator          = instruction.operatorFromString(parts[0])
+        // 			operand, labelref = labelFromString(parts[1])
+        //
+        // 		else: # (label operator)
+        // 			label    = parts[0]
+        // 			operator = instruction.operatorFromString(parts[1])
+        //
+        // 	elif len(parts) == 3: # (label operator operand)
+        // 		label             = parts[0]
+        // 		operator          = instruction.operatorFromString(parts[1])
+        // 		operand, labelref = labelFromString(parts[2])
+        //
+        // 	# DAT or instruction?
+        // 	if operator == instruction.DAT:
+        // 		operator = operand
+        // 		operand  = None
+        //
+        // 	return label, operator, operand, labelref
+        return none //TODO
+    }
 }
 
 
