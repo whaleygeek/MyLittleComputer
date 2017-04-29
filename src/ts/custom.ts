3c1de10d303d35eeed42577fe0f588181a8512ad
David Whale <david@thinkingbinaries.com>
2017-04-29 14:40:31 +0100

    Function signature guesses for mlc_loader

--- src/ts/custom.ts
+++ src/ts/custom.ts
@@ -863,35 +863,37 @@ namespace mlc_parser {
 //----- LOADER ----------------------------------------------------------------
 
 namespace mlc_loader {
+    // # Load numeric data into memory
+    // # Useful for loading a 'binary' file into the simulator
 
-// # Load numeric data into memory
-// # Useful for loading a 'binary' file into the simulator
-//
-// def load(filename, memory, startaddr=0):
-//     """Load from a file into memory"""
-//
-//     f = open(filename, "rt")
-//     addr = startaddr
-//
-//     while True:
-//         instr = io.read(file=f)
-//         if instr != None:
-//             memory[addr] = instr
-//             addr += 1
-//         else:
-//             break
-//
-//     f.close()
-//
-//
-// def showmem(memory, start=0, end=None):
-//     """Show a range of a memory region"""
-//
-//     print("MEMORY:")
-//     if end == None:
-//         end = len(memory)
-//     for addr in range(start, end):
-//         print(str(addr) + " " + str(memory[addr]))
+    function load(filename:string, memory:int_array, start_addr:int=0):void {
+        // def load(filename, memory, startaddr=0):
+        //     """Load from a file into memory"""
+        //
+        //     f = open(filename, "rt")
+        //     addr = startaddr
+        //
+        //     while True:
+        //         instr = io.read(file=f)
+        //         if instr != None:
+        //             memory[addr] = instr
+        //             addr += 1
+        //         else:
+        //             break
+        //
+        //     f.close()
+    }
+
+    function show_mem(memory:int_array, start:int=0, end:int=none):void {
+        // def showmem(memory, start=0, end=None):
+        //     """Show a range of a memory region"""
+        //
+        //     print("MEMORY:")
+        //     if end == None:
+        //         end = len(memory)
+        //     for addr in range(start, end):
+        //         print(str(addr) + " " + str(memory[addr]))
+    }
 }
 
 
